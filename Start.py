bdf291421b90594bfda1ec19c306f9f22f3e1873
Miranda <downimir@students.zhaw.ch>
2025-04-10 12:35:39 +0200

    startseite bearbeitet

---
+++ Start.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("GlucoBuddy")
+st.info("""Diese App ersetzt keine medizinische Beratung. Änderungen Ihrer Therapie sollten nur in Absprache mit Ihrem Arzt erfolgen.""")
