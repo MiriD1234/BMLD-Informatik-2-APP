0e3694895e4a21a6aa33e51b94691e7b0cafb2b3
Miranda <downimir@students.zhaw.ch>
2025-04-14 19:24:29 +0200

    buttons hinzugefügt

--- Start.py
+++ Start.py
@@ -1,4 +1,11 @@
 import streamlit as st
 
 st.title("GlucoBuddy")
+
+if st.button("🧮 Insulinbolus-Rechner"):
+    st.switch_page("pages/2_Insulinbolus-Rechner.py")
+
+if st.button("⚙️ Einstellungen"):
+    st.switch_page("pages/1_Einstellungen.py")
+
 st.info("""Diese App ersetzt keine medizinische Beratung. Änderungen Ihrer Therapie sollten nur in Absprache mit Ihrem Arzt erfolgen.""")
