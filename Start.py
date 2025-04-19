876b7831ac6ef12092ad770f6d7168aff3344daa
Miranda <downimir@students.zhaw.ch>
2025-04-19 16:00:16 +0200

    Button Blutzuckertagebuch hinzugefügt

--- Start.py
+++ Start.py
@@ -24,13 +24,13 @@ import streamlit as st
 
 st.title("GlucoBuddy")
 
+if st.button("📖 Blutzuckertagebuch"):
+    st.switch_page("pages/3_Blutzuckertagebuch.py")
+
 if st.button("🧮 Insulinbolus-Rechner"):
     st.switch_page("pages/2_Insulinbolus-Rechner.py")
 
 if st.button("⚙️ Einstellungen"):
     st.switch_page("pages/1_Einstellungen.py")
-
-if st.button("📖 Blutzuckertagebuch"):
-    st.switch_page("pages/3_Blutzuckertagebuch.py")
 
 st.info("""Diese App ersetzt keine medizinische Beratung. Änderungen Ihrer Therapie sollten nur in Absprache mit Ihrem Arzt erfolgen.""")
